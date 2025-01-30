0294f5af449ac2c9c35f22d2f6d5b9f2346bf7f9
anubis <schmidte@list.ru>
2025-01-30 16:23:28 +0300

    +

--- main.cpp
+++ main.cpp
@@ -9,12 +9,15 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QTranslator  myappTranslator;
 
-    QString tr1 = a.applicationDirPath();
+    QString      tr1 = a.applicationDirPath();
+
     myappTranslator.load("TestConvert_" + QLocale::system().name(), tr1);
 
     a.installTranslator(&myappTranslator);
 
+
     MainWindow w;
+
 
     w.show();
 
