eaaebd636a4e5188b5d0ef71ebcab08d8252886e
anubis <schmidte@list.ru>
2025-07-15 14:23:21 +0300

    fix

--- csvdialog.cpp
+++ csvdialog.cpp
@@ -5,7 +5,9 @@
 #include <QTextCodec>
 
 CSVDialog::CSVDialog(QWidget *parent)
-        : QDialog(parent), ui(new Ui::CSVDialog) {
+    : QDialog(parent)
+    , ui(new Ui::CSVDialog)
+{
     ui->setupUi(this);
     connect(ui->cbEncoding, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
             this, &CSVDialog::update_table);
@@ -30,72 +32,87 @@ CSVDialog::CSVDialog(QWidget *parent)
      * }*/
 }
 
-CSVDialog::~CSVDialog() {
+CSVDialog::~CSVDialog()
+{
     delete ui;
 }
 
 
-QString CSVDialog::result() {
-    const QString separator = QString(ui->cbCSVSeparator->currentText());
-    const QString textDelimiter = QString("\"");
-    QTextCodec *codec = QTextCodec::codecForName(ui->cbEncoding->currentText().toLocal8Bit());
+QString CSVDialog::result()
+{
+    const QString      separator     = QString(ui->cbCSVSeparator->currentText() );
+    const QString      textDelimiter = QString("\"");
+    QTextCodec         *codec        = QTextCodec::codecForName(ui->cbEncoding->currentText().toLocal8Bit() );
 
     QList<QStringList> readData = QtCSV::Reader::readToList(path, separator, textDelimiter, codec);
 
-    if (readData.isEmpty()) {
-        return (QString());
+    if (readData.isEmpty() )
+    {
+        return(QString() );
     }
 
 
     QString text = "";
 
 
-    for (int i = ui->firstQuestion->value() - 1; i < readData.count(); i++) {
+    for (int i = ui->firstQuestion->value() - 1; i < readData.count(); i++)
+    {
         // theme
-        if (ui->cbCategory->currentIndex() > 0 && ui->cbCategory->currentIndex() <= readData.at(i).count()) {
-            text += tr("\n@%1").arg(readData.at(i).at(ui->cbCategory->currentIndex() - 1));
+        if (ui->cbCategory->currentIndex() > 0 && ui->cbCategory->currentIndex() <= readData.at(i).count() )
+        {
+            text += tr("\n@%1").arg(readData.at(i).at(ui->cbCategory->currentIndex() - 1) );
         }
 
         // question
-        if (ui->cbQuestionCol->currentIndex() > 0 && ui->cbQuestionCol->currentIndex() <= readData.at(i).count()) {
-            text += tr("\n?%1").arg(readData.at(i).at(ui->cbQuestionCol->currentIndex() - 1));
+        if (ui->cbQuestionCol->currentIndex() > 0 && ui->cbQuestionCol->currentIndex() <= readData.at(i).count() )
+        {
+            text += tr("\n?%1").arg(readData.at(i).at(ui->cbQuestionCol->currentIndex() - 1) );
         }
 
         //answers
         if (
-                (ui->cbAnswerFirst->currentIndex() > 0)
-                && (ui->cbAnswerFirst->currentIndex() < ui->cbAnswerLast->currentIndex())
-                && (ui->cbAnswerLast->currentIndex() <= readData.at(i).count())
-                && (ui->cbCorrect->currentIndex() > 0) && (ui->cbCorrect->currentIndex() <= readData.at(i).count())) {
-            int correct = readData.at(i).at(ui->cbCorrect->currentIndex()-1).toInt()-1;
-            for (int j=0; j<=ui->cbAnswerLast->currentIndex()-ui->cbAnswerFirst->currentIndex();j++){
-                if (j==correct)
-                    text+=tr("\n+%1").arg(readData.at(i).at(ui->cbAnswerFirst->currentIndex()+j-1));
-                else
-                    text+=tr("\n-%1").arg(readData.at(i).at(ui->cbAnswerFirst->currentIndex()+j-1));
-            }
+            (ui->cbAnswerFirst->currentIndex() > 0)
+            && (ui->cbAnswerFirst->currentIndex() < ui->cbAnswerLast->currentIndex() )
+            && (ui->cbAnswerLast->currentIndex() <= readData.at(i).count() )
+            && (ui->cbCorrect->currentIndex() > 0) && (ui->cbCorrect->currentIndex() <= readData.at(i).count() ) )
+        {
+            int correct = readData.at(i).at(ui->cbCorrect->currentIndex() - 1).toInt() - 1;
 
-        } else if ((ui->cbCorrect->currentIndex() > 0) && (ui->cbCorrect->currentIndex() <= readData.at(i).count())) {
-            text+=tr("\n+%1").arg(readData.at(i).at(ui->cbCorrect->currentIndex()-1));
+
+            for (int j = 0; j <= ui->cbAnswerLast->currentIndex() - ui->cbAnswerFirst->currentIndex(); j++)
+            {
+                if (j == correct)
+                {
+                    text += tr("\n+%1").arg(readData.at(i).at(ui->cbAnswerFirst->currentIndex() + j - 1) );
+                } else {
+                    text += tr("\n-%1").arg(readData.at(i).at(ui->cbAnswerFirst->currentIndex() + j - 1) );
+                }
+            }
+        } else if ( (ui->cbCorrect->currentIndex() > 0) && (ui->cbCorrect->currentIndex() <= readData.at(i).count() ) )
+        {
+            text += tr("\n+%1").arg(readData.at(i).at(ui->cbCorrect->currentIndex() - 1) );
         }
     }
-    return text;
+
+    return(text);
 } // CSVDialog::result
 
 
-void CSVDialog::update_table() {
-    const QString separator = QString(ui->cbCSVSeparator->currentText());
-    const QString textDelimiter = QString("\"");
-    QTextCodec *codec = QTextCodec::codecForName(ui->cbEncoding->currentText().toLocal8Bit());
+void CSVDialog::update_table()
+{
+    const QString      separator     = QString(ui->cbCSVSeparator->currentText() );
+    const QString      textDelimiter = QString("\"");
+    QTextCodec         *codec        = QTextCodec::codecForName(ui->cbEncoding->currentText().toLocal8Bit() );
 
     QList<QStringList> readData = QtCSV::Reader::readToList(path, separator, textDelimiter, codec);
 
-    if (readData.isEmpty()) {
+    if (readData.isEmpty() )
+    {
         ui->table->setRowCount(1);
         ui->table->setColumnCount(1);
 
 
-        QTableWidgetItem *newItem = new QTableWidgetItem(tr("empty"));
+        QTableWidgetItem *newItem = new QTableWidgetItem(tr("empty") );
 
 
         ui->table->setItem(0, 0, newItem);
@@ -104,16 +121,19 @@ void CSVDialog::update_table() {
     }
 
     ui->table->setRowCount(4);
-    ui->table->setColumnCount(readData.at(0).count());
+    ui->table->setColumnCount(readData.at(0).count() );
 
-    for (int i = 0; i < 4; i++) {
-        if (i > readData.count()) {
+    for (int i = 0; i < 4; i++)
+    {
+        if (i > readData.count() )
+        {
             ui->table->setRowCount(i);
             break;
         }
 
-        for (int j = 0; j < readData.at(i).count(); j++) {
-            QTableWidgetItem *newItem = new QTableWidgetItem(readData.at(i).at(j));
+        for (int j = 0; j < readData.at(i).count(); j++)
+        {
+            QTableWidgetItem *newItem = new QTableWidgetItem(readData.at(i).at(j) );
 
 
             ui->table->setItem(i, j, newItem);
@@ -124,31 +144,37 @@ void CSVDialog::update_table() {
     QStringList cols;
 
 
-    for (int j = 0; j <= readData.at(0).count(); j++) {
+    for (int j = 0; j <= readData.at(0).count(); j++)
+    {
         cols << tr("%1").arg(j);
     }
 
-    if (readData.at(0).count() > ui->cbQuestionCol->count()) {
+    if (readData.at(0).count() > ui->cbQuestionCol->count() )
+    {
         ui->cbQuestionCol->clear();
         ui->cbQuestionCol->addItems(cols);
     }
 
-    if (readData.at(0).count() > ui->cbAnswerFirst->count()) {
+    if (readData.at(0).count() > ui->cbAnswerFirst->count() )
+    {
         ui->cbAnswerFirst->clear();
         ui->cbAnswerFirst->addItems(cols);
     }
 
-    if (readData.at(0).count() > ui->cbAnswerLast->count()) {
+    if (readData.at(0).count() > ui->cbAnswerLast->count() )
+    {
         ui->cbAnswerLast->clear();
         ui->cbAnswerLast->addItems(cols);
     }
 
-    if (readData.at(0).count() > ui->cbCategory->count()) {
+    if (readData.at(0).count() > ui->cbCategory->count() )
+    {
         ui->cbCategory->clear();
         ui->cbCategory->addItems(cols);
     }
 
-    if (readData.at(0).count() > ui->cbCorrect->count()) {
+    if (readData.at(0).count() > ui->cbCorrect->count() )
+    {
         ui->cbCorrect->clear();
         ui->cbCorrect->addItems(cols);
     }
